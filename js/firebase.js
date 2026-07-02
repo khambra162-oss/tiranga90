585f74f749a8123ac83c3b64b2779014360be1f6
khambra162-oss <khambra162@gmail.com>
2026-07-02 19:04:30 +0530

    Create firebase.js

---
+++ js/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDwchlmMXcJFHES2Kh82FNx_1xahHyU_dU",
+  authDomain: "tiranga90-game.firebaseapp.com",
+  projectId: "tiranga90-game",
+  storageBucket: "tiranga90-game.firebasestorage.app",
+  messagingSenderId: "121268626051",
+  appId: "1:121268626051:web:6c1459510b476cbaa931b6"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export { auth };
